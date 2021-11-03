a0459283feeace66d75321d958a5f512a1039454
oleg634996 <olegkolodiy22@gmail.com>
2021-11-03 21:52:04 +0200

    v3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,27 @@
 import { throttle } from "lodash";
 
-const form = document.querySelector(".feedback-form");
-
+const form = document.querySelector(".feedback-form")
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const formData = new FormData(form)
-    formData.forEach((value, name) => {
-        console.log(name, value)
-    })
-    localStorage.removeItem(LOCALSTORAGE_KEY)
-    form.reset()
-  
-})
-
- 
 form.addEventListener('input', throttle(event => {
     let filters = localStorage.getItem(LOCALSTORAGE_KEY);
     filters = filters ? JSON.parse(filters) : {};
     filters[event.target.name] = event.target.value;
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(filters));
 },500))
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(form)
+    formData.forEach((value, name) => {
+        console.log({name, value})
+       
+    })
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+    form.reset()
+})
+console.log()
 
 function onForm() {
      let filters = localStorage.getItem(LOCALSTORAGE_KEY);
