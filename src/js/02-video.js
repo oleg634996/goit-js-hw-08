5dbd309152294103e0405a0753839f1cb29cbdd7
oleg634996 <olegkolodiy22@gmail.com>
2021-11-06 20:28:20 +0200

    v5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,13 +6,20 @@ const player = new Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time"
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY)
 
-
-
-player.setCurrentTime(currentTime)
-
-player.on('timeupdate',throttle(data => localStorage.setItem(LOCALSTORAGE_KEY,data.seconds),1000))
-
-    if (localStorage !== 0) {
-        localStorage.removeItem(LOCALSTORAGE_KEY)
-    }
+  
+player.on('timeupdate', throttle(() => {
+  player.getCurrentTime()
+        .then(function (seconds) {
+            localStorage.setItem(LOCALSTORAGE_KEY, seconds)
+        }).catch(function (error) {
+            return error
+        });
+}, 1000));
+    
+    if (currentTime) {
+        player.setCurrentTime(currentTime)
+            .catch(function(error) {
+        console.log('RangeError', error)
+});
+};
 
